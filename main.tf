aaf875fdae4250deb41b8660bef3b5741767a660
Ondrej Sika <ondrej@ondrejsika.com>
2024-11-19 10:20:26 +0100

    feat: Setup AWS provider and example bucket

--- main.tf
+++ main.tf
@@ -8,9 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region     = "eu-central-1"
-  access_key = "AKIAS66YWWD3HXNHRSGD"
-  secret_key = "zzx5FcPj4uoORGDvT4FDekoXcHKyazp"
+  region = "eu-central-1"
 }
 
 resource "aws_s3_bucket" "example" {
